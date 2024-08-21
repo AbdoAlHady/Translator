10fdcf81ad4e29d7cede742bec07de9f6661df14
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-08-21 17:35:04 +0300

    - added flutter generate assets to pub - added class that have all paths of assets images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for imagesArabic
+  /// assets/images/arabic.png
+  static const String imagesArabic = "assets/images/arabic.png";
+
+  /// Assets for imagesBritain
+  /// assets/images/britain.png
+  static const String imagesBritain = "assets/images/britain.png";
+
+  /// Assets for imagesChina
+  /// assets/images/china.png
+  static const String imagesChina = "assets/images/china.png";
+
+  /// Assets for imagesFrance
+  /// assets/images/france.png
+  static const String imagesFrance = "assets/images/france.png";
+
+  /// Assets for imagesGermany
+  /// assets/images/germany.png
+  static const String imagesGermany = "assets/images/germany.png";
+
+  /// Assets for imagesItaly
+  /// assets/images/italy.png
+  static const String imagesItaly = "assets/images/italy.png";
+
+  /// Assets for imagesRussia
+  /// assets/images/russia.png
+  static const String imagesRussia = "assets/images/russia.png";
+
+  /// Assets for imagesSpain
+  /// assets/images/spain.png
+  static const String imagesSpain = "assets/images/spain.png";
+
+  /// Assets for imagesTalkglobalSnippet
+  /// assets/images/talkglobal_snippet.png
+  static const String imagesTalkglobalSnippet = "assets/images/talkglobal_snippet.png";
+
+  /// Assets for imagesUsa
+  /// assets/images/usa.png
+  static const String imagesUsa = "assets/images/usa.png";
+
+  /// Assets for imagesWorldmap
+  /// assets/images/worldmap.png
+  static const String imagesWorldmap = "assets/images/worldmap.png";
+}
+
