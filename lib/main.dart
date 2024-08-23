804fbc9167f06f5a71f0ddae82052da0c230dd14
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-08-23 03:39:16 +0300

    - fix font bux

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:translator_app/core/helper/env_variables.dart';
 import 'package:translator_app/translator_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   await EnvVariables().init();
   runApp(const TranslatorApp());
 }
